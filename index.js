d8689732a822d1d77f15b83d9c6867f73dddd2b5
Abubokkor98 <mail.abubokkor@gmail.com>
2024-12-22 16:03:12 +0600

    get all food from db

--- index.js
+++ index.js
@@ -43,6 +43,11 @@ async function run() {
       const result = await foodCollection.insertOne(newFood);
       res.send(result);
     });
+    // get all foods from db
+    app.get("/foods", async (req, res) => {
+      const result = await foodCollection.find().toArray();
+      res.send(result);
+    });
 
     // Send a ping to confirm a successful connection
     // await client.db("admin").command({ ping: 1 });
